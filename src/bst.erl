fb29109b8ea0e8fe9ae7f3cc25983545d4425725
dancewithheart <dancingwithopenheart@gmail.com>
2025-09-22 02:51:56 +0200

    BST add elements and more props

--- src/bst.erl
+++ src/bst.erl
@@ -4,7 +4,8 @@
 %% by Andrew W. Appel
 %% https://softwarefoundations.cis.upenn.edu/vfa-current/SearchTree.html
 -module(bst).
--export([empty/0, new/2, new/4, bound/2, lookup/3, insert/3, equal/2, forall/2, isBST/1]).
+-export([empty/0, new/2, new/4, bound/2, lookup/3, insert/3, equal/2,
+    forall/2, isBST/1, elements/1]).
 -record(tree, {left, key, value, rigt}).
 % #tree{left = L, key = K, value = V, rigt = R}
 % tree ::= bstEmpty | l: tree, k: K, v: V, r: tree
@@ -50,9 +51,16 @@ forall(_, empty) -> true;
 forall(P, #tree{left = L, key = K, value = V, rigt = R}) ->
   P(K,V) andalso forall(P, L) andalso forall(P, R).
 
-% property of BST
+% check if #tree is proper BST
 isBST(empty) -> true;
 isBST(#tree{left = L, key = K, rigt = R}) ->
     forall(fun(K2,_) -> K2 < K end, L)
     andalso forall(fun(K2,_) -> K2 > K end, R)
     andalso isBST(L) andalso isBST(R).
+
+% converts BST to association list - in order travrsal
+elements(T) -> elements(T, []).
+
+elements(empty, Acc) -> Acc;
+elements(#tree{left = L, key = K, value = V, rigt = R}, Acc) ->
+    elements(L, [{K,V} | elements(R, Acc)]).
--- test/bst_tests.erl
+++ test/bst_tests.erl
@@ -14,7 +14,8 @@ tree_test_() ->
         test_lookup_missing_key(BST),
         test_bound_missing_key(BST),
         test_isBST_holds_for_valid_BST(BST),
-        test_isBST_detect_invalid_BST()].
+        test_isBST_detect_invalid_BST(),
+        test_elements_create_assoc_list(BST)].
 
 test_insert(BST) ->
   ?_assertEqual(bst:insert(5, "five", (bst:insert(2,"two",(bst:insert(4,"four", bst:empty()))))), BST).
@@ -36,9 +37,11 @@ test_isBST_detect_invalid_BST() ->
       bst:new(bst:empty(), 2, "two", bst:empty())
     ))
 ).
+test_elements_create_assoc_list(BST) ->
+  ?_assertEqual([{2, "two"}, {4, "four"}, {5, "five"}], bst:elements(BST)).
 
-validBst(T) ->
-    ?LET(L, list(T), list_to_bst(L)).
+validBst(Gen) ->
+    ?LET(L, list(Gen), list_to_bst(L)).
 
 list_to_bst([]) -> bst:empty();
 list_to_bst(XS) ->
@@ -47,8 +50,32 @@ list_to_bst(XS) ->
 % if we insert all elements using bst:insert then we have valid BST
 % forall k,v,t. isBST(t) => isBST(insert(k,v,t))
 prop_BST_after_insert_is_BST() ->
-  ?FORALL(L, validBst(integer()), bst:isBST(L)).
+  ?FORALL(L, validBst(integer()),
+  bst:isBST(L)).
 
-% TODO lookup d k empty_tree = d
-% TODO lookup d k (insert k v t) = v
+% lookup d k empty_tree = d
+prop_lookup_empty_BST_gives_default() ->
+  ?FORALL({D,K}, {string(), integer()},
+  bst:lookup(D, K, bst:empty()) == D ).
+
+% lookup d k (insert k v t) = v
+prop_lookup_inserted_gives_inserted_elem() ->
+  ?FORALL({L,D,V,K}, {validBst(integer()),string(),string(), integer()},
+  bst:lookup(D, K, (bst:insert(K, V, L))) == V ).
+
 % if k ≠ k' => lookup d k' (insert k v t) = lookup d k' t  
+
+prop_insert_different_key_not_affect_lookup() ->
+  ?FORALL(
+    {L,D,V,K,K2},
+    {validBst(integer()),string(),string(),integer(),integer()},
+    ?IMPLIES(
+      K =/= K2,
+      bst:lookup(D, K2, (bst:insert(K, V, L))) == bst:lookup(D, K2, L) )).
+
+% lookup d k' (insert k v (insert k v' t)) = lookup d k' (insert k v t)
+% lookup d k' (insert k (lookup d k t) t) = lookup d k' t.
+%  k1 ≠ k2 =>
+%   lookup d k' (insert k1 v1 (insert k2 v2 t))
+%    =
+%   lookup d k' (insert k2 v2 (insert k1 v1 t))
