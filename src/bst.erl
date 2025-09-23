13ddbf4528d7bdce7bdfea1f96ec9e52c414cfd8
dancewithheart <dancingwithopenheart@gmail.com>
2025-09-23 17:25:46 +0200

    add types and spec to BST

--- src/bst.erl
+++ src/bst.erl
@@ -10,66 +10,79 @@
     forall/2,
     elements/1, from_list/1, 
     is_bst/1, equal/2]).
--record(tree, {left, key, value, rigt}).
-% #tree{left = L, key = K, value = V, rigt = R}
-% tree ::= bstEmpty | l: tree, k: K, v: V, r: tree
+-record(tree, {left, key, value, right}).
 
+-type bst(K, V) :: empty | #tree{
+    left :: bst(K, V),
+    key :: K,
+    value :: V,
+    right :: bst(K, V)}.
+
+-spec empty() -> bst(_K,_V).
 empty() -> empty.
 
-new(L,K,V,R) -> #tree{left = L, key = K, value = V, rigt = R}.
+-spec new(bst(K,V), K, V, bst(K,V)) -> bst(K,V).
+new(L,K,V,R) -> #tree{left = L, key = K, value = V, right = R}.
 
-% new(T,V) -> bst(T)
+-spec new(K,V) -> bst(K,V).
 new(Key, Value) -> new(empty(), Key, Value, empty()).
 
 % check if K is bound in BST
+-spec bound(K, bst(K,_V)) -> boolean().
 bound(_, empty) -> false;
 bound(K, #tree{left = L, key = K2}) when K < K2 -> bound(K, L);
-bound(K, #tree{key = K2, rigt = R}) when K > K2 -> bound(K, R);
+bound(K, #tree{key = K2, right = R}) when K > K2 -> bound(K, R);
 bound(_,_) -> true.
 
-% lookup(V, K, tree): bool()
 % value bound to K in tree, or default value D
+-spec lookup(V, K, bst(K,V)) -> V.
 lookup(D, _K, empty) -> D;
 lookup(D, K, #tree{left = L, key = K2}) when K < K2 -> lookup(D,K,L);
-lookup(D, K, #tree{key = K2, rigt = R}) when K > K2 -> lookup(D,K,R);
+lookup(D, K, #tree{key = K2, right = R}) when K > K2 -> lookup(D,K,R);
 lookup(_, _, #tree{value = V}) -> V.
 
+-spec insert(V, K, bst(K,V)) -> bst(K,V).
 insert(K, V, empty) -> new(K,V);
-insert(K, V, #tree{left = L, key = K2, value = V2, rigt = R}) when K < K2
+insert(K, V, #tree{left = L, key = K2, value = V2, right = R}) when K < K2
   -> new(insert(K, V, L), K2, V2, R);
-insert(K, V, #tree{left = L, key = K2, value = V2, rigt = R}) when K > K2
+insert(K, V, #tree{left = L, key = K2, value = V2, right = R}) when K > K2
   -> new(L, K2, V2, insert(K, V, R));
-insert(K, V, #tree{left = L, key = _K2, value = _V2, rigt = R})
+insert(K, V, #tree{left = L, key = _K2, value = _V2, right = R})
   -> new(L, K, V, R).
 
+-spec equal(bst(K,V), bst(K,V)) -> boolean().
 equal(empty, empty) -> true;
 equal(#tree{}, empty) -> false;
 equal(empty,  #tree{}) -> false;
-equal( #tree{left = L, key = K, value = V, rigt = R},
-       #tree{left = L2, key = K2, value = V2, rigt = R2}) when K == K2, V == V2
+equal( #tree{left = L, key = K, value = V, right = R},
+       #tree{left = L2, key = K2, value = V2, right = R2}) when K == K2, V == V2
   -> equal(L,L2) andalso equal(R,R2);
 equal(_, _) -> false.
 
 % forall(P: fun(K,V) -> bool(), t: tree()) -> bool()
+-spec forall(any(), bst(_K,_V)) -> boolean().
 forall(_, empty) -> true;
-forall(P, #tree{left = L, key = K, value = V, rigt = R}) ->
+forall(P, #tree{left = L, key = K, value = V, right = R}) ->
   P(K,V) andalso forall(P, L) andalso forall(P, R).
 
 % check if #tree is proper BST
+-spec is_bst(bst(_K,_V)) -> boolean().
 is_bst(empty) -> true;
-is_bst(#tree{left = L, key = K, rigt = R}) ->
+is_bst(#tree{left = L, key = K, right = R}) ->
     forall(fun(K2,_) -> K2 < K end, L)
     andalso forall(fun(K2,_) -> K2 > K end, R)
     andalso is_bst(L) andalso is_bst(R).
 
 % converts BST to association list - in order travrsal
+-spec elements(bst(K,V)) -> [{K,V}].
 elements(T) -> elements(T, []).
 
 elements(empty, Acc) -> Acc;
-elements(#tree{left = L, key = K, value = V, rigt = R}, Acc) ->
+elements(#tree{left = L, key = K, value = V, right = R}, Acc) ->
     elements(L, [{K,V} | elements(R, Acc)]).
 
 % converts list to BST
+-spec from_list([{K,V}]) -> bst(K,V).
 from_list([]) -> bst:empty();
 from_list(XS) ->
   lists:foldl(fun({K, V}, Acc) -> bst:insert(K, V, Acc) end, bst:empty(), XS).
