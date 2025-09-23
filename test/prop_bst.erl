f2910bc6691183f2b3a4d6fef453ef651f0b8591
dancewithheart <dancingwithopenheart@gmail.com>
2025-09-23 16:59:34 +0200

    use BST from_list in test

--- test/prop_bst.erl
+++ test/prop_bst.erl
@@ -3,18 +3,14 @@
 
 %% property tests for Binary Search Tree (BST)
 
-bst_gen(Gen) ->
-    ?LET(L, list(Gen), list_to_bst(L)).
+%%%%%%%%%%%%%%%%%%
+%%% Properties %%%
 
-list_to_bst([]) -> bst:empty();
-list_to_bst(XS) ->
-  lists:foldl(fun(E, Acc) -> bst:insert(E, integer_to_list(E), Acc) end, bst:empty(), XS).
-
-% if we insert all elements using bst:insert then we have valid BST
-% forall k,v,t. is_bst(t) => is_bst(insert(k,v,t))
+% is_bst(t) => is_bst(insert(k,v,t))
 prop_bst_after_insert_is_bst() ->
-  ?FORALL(L, bst_gen(integer()),
-  bst:is_bst(L)).
+  ?FORALL( {L,K,V}, {bst_gen(integer()), integer(), string()},
+    bst:is_bst(bst:insert(K,V,L))
+  ).
 
 % lookup d k empty_tree = d
 prop_lookup_empty_bst_gives_default() ->
@@ -25,17 +21,35 @@ prop_lookup_empty_bst_gives_default() ->
 % lookup d k (insert k v t) = v
 prop_lookup_inserted_gives_inserted_elem() ->
   ?FORALL( {L,D,V,K}, {bst_gen(integer()),string(),string(), integer()},
-    bst:lookup(D, K, (bst:insert(K, V, L))) == V
+    bst:lookup(D, K, (bst:insert(K,V,L))) == V
   ).
 
 % if k ≠ k' => lookup d k' (insert k v t) = lookup d k' t  
 
 prop_insert_different_key_not_affect_lookup() ->
-  ?FORALL( {L,D,V,K,K2}, {bst_gen(integer()), string(), string(), integer(), integer()},
+  ?FORALL( {L,D,V,K,K2},
+    {bst_gen(integer()), string(), string(), integer(), integer()},
     ?IMPLIES( K =/= K2,
-      bst:lookup(D, K2, (bst:insert(K, V, L))) == bst:lookup(D, K2, L)
+      bst:lookup(D, K2, (bst:insert(K,V,L))) == bst:lookup(D,K2,L)
     )).
 
+%%%%%%%%%%%%%%%%%%
+%%% Generators %%%
+
+bst_gen(Gen) ->
+    ?LET(L, list(Gen), list_to_bst(L)).
+
+%%%%%%%%%%%%%%%
+%%% Helpers %%%
+
+list_to_bst([]) -> bst:empty();
+list_to_bst(XS) ->
+  YS = lists:map(fun(E) -> {E, integer_to_list(E)} end, XS),
+  bst:from_list(YS).
+
+% TODO properties for bound
+
+% TODO
 % lookup d k' (insert k v (insert k v' t)) = lookup d k' (insert k v t)
 % lookup d k' (insert k (lookup d k t) t) = lookup d k' t.
 %  k1 ≠ k2 =>
