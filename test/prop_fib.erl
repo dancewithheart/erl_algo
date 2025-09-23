fadd38150114b5f85091abcdf8eff6884bda9e54
dancewithheart <dancingwithopenheart@gmail.com>
2025-09-23 17:03:32 +0200

    clean up fib props - drop rem 30

--- test/prop_fib.erl
+++ test/prop_fib.erl
@@ -2,20 +2,29 @@
 -import(fib, [fib/1]).
 -include_lib("proper/include/proper.hrl").
 
+%%%%%%%%%%%%%%%%%%
+%%% Properties %%%
+
 prop_sum_two_previous() ->
   ?FORALL(N, non_neg_integer(),
     fib(N) + fib(N+1) == fib(N+2)
   ).
 
 prop_eq_naive_impl() ->
-  ?FORALL(N, non_neg_integer_below(30),
-    fib(N rem 30) == fib_oracle(N rem 30)
+  ?FORALL(N, non_neg_integer_below(25),
+    fib(N) == fib_oracle(N)
   ).
+
+%%%%%%%%%%%%%%%%%%
+%%% Generators %%%
+
+non_neg_integer_below(N) ->
+    ?LET(L, non_neg_integer(), L rem N).
+
+%%%%%%%%%%%%%%%
+%%% Helpers %%%
 
 % naive fibonacci O(2^n)
 fib_oracle(0) -> 1;
 fib_oracle(1) -> 1;
 fib_oracle(N) when N > 1 -> fib_oracle(N-1) + fib_oracle(N-2).
-
-non_neg_integer_below(N) ->
-    ?LET(L, non_neg_integer(), L rem N).
