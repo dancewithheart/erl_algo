a533d573550dda8976f3f1435496d8dc98c5fb7c
dancewithheart <dancingwithopenheart@gmail.com>
2025-09-26 02:55:30 +0200

    red black tree - test property is_bst and is_red_black

--- test/prop_red_black_tree.erl
+++ test/prop_red_black_tree.erl
@@ -4,11 +4,15 @@
 %%%%%%%%%%%%%%%%%%
 %%% Properties %%%
 
-% TODO
-% prop_after_insert_is_bst() ->
-%   ?FORALL( {L,K,V}, {red_black_gen(integer()), integer(), string()},
-%     red_black_tree:is_bst(red_black_tree:insert(K,V,L))
-%   ).
+prop_after_insert_is_red_black() ->
+  ?FORALL( {L,V,K}, {red_black_gen(integer()), string(), integer()},
+    red_black_tree:is_red_black(red_black_tree:insert(K,V,L))
+  ).
+
+prop_after_insert_is_bst() ->
+  ?FORALL( {L,V,K}, {red_black_gen(integer()), string(), integer()},
+    red_black_tree:is_bst(red_black_tree:insert(K,V,L))
+  ).
 
 % lookup d k empty_tree = d
 prop_lookup_empty_gives_default() ->
@@ -54,7 +58,7 @@ prop_insert_different_key_not_affect_bound() ->
       red_black_tree:bound(K2, red_black_tree:insert(K,V,L)) == red_black_tree:bound(K2,L)
     )).
 
-% properties for bound amd lookup
+% properties for bound and lookup
 
 % bound k t == false => lookup d k t == d
 prop_if_not_bound_then_lookup_default() ->
@@ -74,39 +78,16 @@ prop_lookup_not_default_then_bound() ->
     end
   ).
 
-% prop_red_black_tree_after_balance_is_bst() ->
-%   ?FORALL( {L,R,K,V,C}, {red_black_gen(integer()), red_black_gen(integer()), integer(), string(), color_gen()},
-%     red_black_tree:is_bst(red_black_tree:balance(C,L,K,V,R))
-%   ).
-
 %%%%%%%%%%%%%%%%%%
 %%% Generators %%%
 
 red_black_gen(Gen) ->
   ?LET(L, list(Gen), list_to_red_black_tree(L)).
 
-% color_gen() ->
-%   ?LET(B, boolean(), bool_to_color(B)).
-
 %%%%%%%%%%%%%%%
 %%% Helpers %%%
-
-% bool_to_color(true) -> red;
-% bool_to_color(false) -> black.
 
 list_to_red_black_tree([]) -> red_black_tree:empty();
 list_to_red_black_tree(XS) ->
   YS = lists:map(fun(E) -> {E, integer_to_list(E)} end, XS),
   red_black_tree:from_list(YS).
-
-%% TODO balance preserves ForallT : ForallT P l → ForallT P r → P k v → ForallT P (balance c l k v r).
-% ins preserves BST : BST t → BST (ins k v t).
-
-% lookup spec
-% lookup d k empty_tree = d
-% lookup d k (insert k v t) = v
-% lookup d k' (insert k v t) = lookup d k' t       if k ≠ k'
-
-% red-black invariants hold:
-% Local Invariant: No red node has a red child.
-% Global Invariant: Every path from the root to a leaf has the same number of black nodes.
