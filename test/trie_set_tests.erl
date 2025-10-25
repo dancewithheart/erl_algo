2971a064f2ec9395543f43646ea308da1f8fe8c6
dancewithheart <dancingwithopenheart@gmail.com>
2025-10-25 21:05:57 +0200

    Revert "add Set implemented using Trie"
    This reverts commit 24fbe65bc4fc3736f261f8156e9df994f270f86a.

--- test/trie_set_tests.erl
+++
@@ -1,9 +0,0 @@
--module(trie_set_tests).
--include_lib("eunit/include/eunit.hrl").
--import(trie_set, [member/2, new/0, add/2, from_list/1]).
-
-trie_set_test_() ->
-  [
-    ?_assertEqual( false, member("foo", new()) ),
-    ?_assertEqual( true, member("foo", add("foo", new()) ) )
-  ].
